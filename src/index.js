a7e9be89f823124335a066cbf1695b50c1e4966b
Eredin <davidtamliani@yandex.ru>
2022-01-18 20:03:30 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('js')
+import { validateIp } from './helpers/index.js';
+
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  if (validateIp(ipInput.value)) {
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_3Hk6cKlPgj5M8dWbJAToHfNi2LLWT&ipAddress=
+    ${ipInput.value}`)
+      .then((response) => response.json())
+      .then(setInfo);
+  }
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
+
+function setInfo (mapData) {
+  console.log(mapData);
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+  timezoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
+}
